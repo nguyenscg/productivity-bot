6eba90cef075b48740a32e5f9e5c2848ee0c7a0f
Sophia Nguyen <nguyencsg@gmail.com>
2025-08-08 19:09:59 -0500

    setup on_ready start up message

--- bot.py
+++ bot.py
@@ -12,8 +12,10 @@ CHANNEL_ID = 1403526849371574452
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.command()
-async def test(ctx):
-    print("-R- is running!")
-    await ctx.send(f"-R- is running now!")
+async def on_ready():
+    channel = bot.get_channel(CHANNEL_ID)
+    print("-R- is ready!")
+
+    await channel.send(f"Hello! -R- is here!")
 
 bot.run(TOKEN)
